7087f591255d7856c8ac60344c987ecdc53e1bda
fathlaziss <71271714+fathlaziss@users.noreply.github.com>
2021-01-26 21:36:30 +0700

    add navigation page

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,16 +1,48 @@
 part of 'pages.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int selectedPage = 0;
+  PageController pageController = PageController(initialPage: 0);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           SafeArea(
-              child: Align(
-            alignment: Alignment.bottomCenter,
-            child: CustomBottomNavBar(),
-          )),
+            child: PageView(
+              controller: pageController,
+              onPageChanged: (index) {
+                setState(() {
+                  selectedPage = index;
+                });
+              },
+              children: [
+                Text("Home"),
+                Text("Inventory"),
+                Text("Profile"),
+              ],
+            ),
+          ),
+          SafeArea(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: CustomBottomNavBar(
+                selectedIndex: selectedPage,
+                onTap: (index) {
+                  setState(() {
+                    selectedPage = index;
+                  });
+                  pageController.jumpToPage(selectedPage);
+                },
+              ),
+            ),
+          ),
         ],
       ),
     );
--- lib/ui/widgets/custom_bottom_navbar.dart
+++ lib/ui/widgets/custom_bottom_navbar.dart
@@ -1,6 +1,11 @@
 part of 'widgets.dart';
 
 class CustomBottomNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int index) onTap;
+
+  CustomBottomNavBar({this.selectedIndex = 0, this.onTap});
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -10,66 +15,110 @@ class CustomBottomNavBar extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.home,
-                  size: 40,
-                  color: mainColor,
-                ),
-                Text(
-                  "Home",
-                  style: GoogleFonts.poppins(
-                    color: mainColor,
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold,
+          GestureDetector(
+            onTap: () {
+              if (onTap != null) {
+                onTap(0);
+              }
+            },
+            child: Container(
+              width: 65,
+              height: 50,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.home,
+                    size: 30,
+                    color: (selectedIndex == 0) ? mainColor : Color(0xFF8E8E93),
                   ),
-                )
-              ],
+                  Text(
+                    "Home",
+                    style: (selectedIndex == 0)
+                        ? GoogleFonts.poppins(
+                            color: mainColor,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                          )
+                        : GoogleFonts.poppins(
+                            color: Color(0xFF8E8E93),
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                          ),
+                  )
+                ],
+              ),
             ),
           ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 85),
-            padding: EdgeInsets.only(top: 2),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  MdiIcons.calendarBlank,
-                  size: 38,
-                  color: Color(0xFF8E8E93),
-                ),
-                Text(
-                  "Inventory",
-                  style: GoogleFonts.poppins(
-                    color: Color(0xFF8E8E93),
-                    fontSize: 12,
-                    fontWeight: FontWeight.w500,
+          GestureDetector(
+            onTap: () {
+              if (onTap != null) {
+                onTap(1);
+              }
+            },
+            child: Container(
+              width: 65,
+              height: 50,
+              margin: EdgeInsets.symmetric(horizontal: 65),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    MdiIcons.calendarBlank,
+                    size: 30,
+                    color: (selectedIndex == 1) ? mainColor : Color(0xFF8E8E93),
                   ),
-                )
-              ],
+                  Text(
+                    "Inventory",
+                    style: (selectedIndex == 1)
+                        ? GoogleFonts.poppins(
+                            color: mainColor,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                          )
+                        : GoogleFonts.poppins(
+                            color: Color(0xFF8E8E93),
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                          ),
+                  )
+                ],
+              ),
             ),
           ),
-          Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.person,
-                  size: 41,
-                  color: Color(0xFF8E8E93),
-                ),
-                Text(
-                  "Student",
-                  style: GoogleFonts.poppins(
-                    color: Color(0xFF8E8E93),
-                    fontSize: 12,
-                    fontWeight: FontWeight.w500,
+          GestureDetector(
+            onTap: () {
+              if (onTap != null) {
+                onTap(2);
+              }
+            },
+            child: Container(
+              width: 65,
+              height: 50,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.person,
+                    size: 30,
+                    color: (selectedIndex == 2) ? mainColor : Color(0xFF8E8E93),
                   ),
-                )
-              ],
+                  Text(
+                    "Student",
+                    style: (selectedIndex == 2)
+                        ? GoogleFonts.poppins(
+                            color: mainColor,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                          )
+                        : GoogleFonts.poppins(
+                            color: Color(0xFF8E8E93),
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                          ),
+                  )
+                ],
+              ),
             ),
           )
         ],
