c722e88f083fb0bf911aa0ae30892aa09892d9e2
fathlaziss <71271714+fathlaziss@users.noreply.github.com>
2021-02-27 13:33:28 +0700

    finishing inventory page

--- lib/ui/pages/inventory_page.dart
+++ lib/ui/pages/inventory_page.dart
@@ -6,18 +6,81 @@ class InventoryPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            margin: EdgeInsets.only(top: 30),
-            child: Text(
-              "All Left Stuff",
-              style: GoogleFonts.poppins(
-                  fontSize: 17,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black),
+          Material(
+            color: Colors.white,
+            elevation: 1,
+            child: Container(
+              height: 44,
+              child: Center(
+                child: Text(
+                  "Inventory",
+                  style: GoogleFonts.poppins(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black),
+                ),
+              ),
             ),
-          )
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(17, 17, 17, 0),
+            child: Wrap(
+              spacing: 24,
+              runSpacing: 24,
+              children: generateInventoryWidgets(context),
+            ),
+          ),
         ],
       ),
     );
+  }
+
+  List<Widget> generateInventoryWidgets(BuildContext context) {
+    double width = (MediaQuery.of(context).size.width - 2 * 24 - 24) / 2;
+    return mockInventory
+        .map(
+          (e) => Container(
+            width: width,
+            height: 115,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+              color: Colors.white,
+              boxShadow: [BoxShadow(blurRadius: 1, color: Colors.black26)],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 44,
+                  width: 44,
+                  margin: EdgeInsets.only(top: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(5),
+                      topRight: Radius.circular(5),
+                    ),
+                    image: DecorationImage(
+                      image: AssetImage(e.picturePath),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 14,
+                ),
+                Text(
+                  e.inventoryName,
+                  style: GoogleFonts.poppins(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black),
+                  maxLines: 1,
+                  overflow: TextOverflow.clip,
+                ),
+              ],
+            ),
+          ),
+        )
+        .toList();
   }
 }
